2e758d2317b4b78a1810a14009e2dd121256bb6b
Achew <artem.kovzik@yandex.ru>
2020-12-05 13:59:20 +0300

    Выполнил практическое задание 7

---
+++ Practice_7.py
@@ -0,0 +1,23 @@
+# Реализовать проект «Операции с комплексными числами».
+# Создайте класс «Комплексное число», реализуйте перегрузку методов сложения и
+# умножения комплексных чисел. Проверьте работу проекта, создав экземпляры класса (комплексные числа) и
+# выполнив сложение и умножение созданных экземпляров. Проверьте корректность полученного результата.
+
+class Complex:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def __add__(self, other):
+        return Complex(self.a + other.a, self.b + other.b)
+
+    def __mul__(self, other):
+        return Complex(self.a * other.a - self.b * other.b, self.a * other.b + self.b * other.a)
+
+    def __str__(self):
+        return f'{self.a} + {self.b}i'
+
+comp_1 = Complex(1,2)
+comp_2 = Complex(3,4)
+print(comp_1 + comp_2)
+print(comp_1 * comp_2)
